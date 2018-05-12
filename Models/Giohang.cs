af7066f5a5a0b37f3f110f823e2b43ae09a20f6a
lecamtung <38534810+lecamtung@users.noreply.github.com>
2018-05-12 10:56:56 +0700

    upload

---
+++ Models/Giohang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBookStore.Models
+{
+    public class Giohang
+    {
+        dbQLBansachDataContext data = new dbQLBansachDataContext();
+
+        public int iMasach { get; set; }
+        public string sTensach { set; get; }
+        public string sAnhbia { get; set; }
+
+        public Double dDongia { get; set; }
+        public int iSoluong { get; set; }
+
+        public Double dThanhtien
+        {
+            get { return iSoluong * dDongia; }
+        }
+        public Giohang(int Masach)
+        {
+            iMasach = Masach;
+            SACH sach = data.SACHes.Single(n => n.Masach == iMasach);
+            sTensach = sach.Tensach;
+            sAnhbia = sach.Hinhminhhoa;
+            dDongia = double.Parse(sach.Dongia.ToString());
+            iSoluong = 1;
+        }
+    }
+
+}
